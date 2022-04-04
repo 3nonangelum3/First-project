bbd1ed163a2931a0dc541b94cc86b84c17805900
3nonangelum3 <n13LogvynenkoIvan13n@gmail.com>
2022-04-04 15:05:11 +0300

    bigger program

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 const numberOfFilms = +prompt("How many films have you watched?", "");
 let personalMovieDB = {
-        count: numberOfFilms,
-        movies: {
-
-        },
-        actors: {},
-        genres: [],
-        private: false
-    };
-const a = prompt("What film?","");
-const b = +prompt("What rate?", "");
-const c = prompt("What film?","");
-const d = +prompt("What rate?", "");
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+let a, b;
+for (let i = 0; i < 2; i++) {
+    do {
+        a = prompt("What film?", "");
+        b = +prompt("What rate?", "");
+    } while (a.length > 50 && a === null && a == "" && b != null);
+    personalMovieDB.movies[a] = b;
+}
+if (personalMovieDB.count < 10) {
+    console.log("a few");
+} else {
+    if (personalMovieDB.count < 30) {
+        console.log("normal");
+    } else {
+        if (personalMovieDB.count >= 30) {
+            console.log("too much");
+        } else {
+            console.log("error");
+        }
+    }
+}
 console.log(personalMovieDB);
